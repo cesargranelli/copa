4b3eac1dbb36b35ed6d166546da5575cb439265a
Cesar Granelli <cesargranelli@gmail.com>
2018-04-14 00:58:11 -0300

    Initial Signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,36 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HomePage } from './../pages/home/home';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { SignupPage } from './../pages/signup/signup';
+
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyAkaxvKWCyJezIU5-0gyRJyJW0uJY9r-Ws",
+  authDomain: "copa-6141d.firebaseapp.com",
+  databaseURL: "https://copa-6141d.firebaseio.com",
+  storageBucket: "copa-6141d.appspot.com",
+  messagingSenderId: "575539116738"
+};
 
 @NgModule({
   declarations: [
+    HomePage,
     MyApp,
-    HomePage
+    SignupPage
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseAppConfig),
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    HomePage,
     MyApp,
-    HomePage
+    SignupPage
   ],
   providers: [
     StatusBar,
