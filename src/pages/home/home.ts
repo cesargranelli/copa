e7e7e734647df699499af9902e662196832eb811
cesargranelli <cesargranelli@gmail.com>
2018-05-04 01:13:11 -0300

    Página Home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { HttpClient } from '@angular/common/http';
 import { Player } from '../../models/player';
@@ -20,20 +20,43 @@ export class HomePage {
     public fireBase: AngularFireDatabase,
     public http: HttpClient,
     public navCtrl: NavController,
-    private _navParams: NavParams
+    private _navParams: NavParams,
+    private _alertCtrl: AlertController,
+    private _loadingCtrl: LoadingController
   ) {
     this._userid = this._navParams.get('userid');
   }
 
   ionViewDidLoad() {
+
+    let loading = this.showLoading();
+
     this.fireBase.database.ref(`users/${this._userid}`).once('value')
       .then((snapshotUser) => {
         this.user = snapshotUser.val();
         this.fireBase.database.ref(`players/${snapshotUser.val().username}`).once('value')
           .then((snapshotPlayer) => {
             this.player = snapshotPlayer.val();
+            loading.dismiss();
           })
       });
+  }
+
+  private showLoading(): Loading {
+    let loading: Loading = this._loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
+
+    return loading;
+  }
+
+  private showAlert(message: string): void {
+    this._alertCtrl.create({
+      message: message,
+      buttons: ['Ok']
+    }).present();
   }
 
 }
